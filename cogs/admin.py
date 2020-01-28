e56947084766c01ca6bc3c9d545af5ebcdf0b9b3
Mike C <michael.e.case@gmail.com>
2020-01-28 02:48:07 -0600

    more DB implementation

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
+from utils.db import Session, User, Promote
+from sqlalchemy import and_
+
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -23,6 +27,37 @@ class Admin(commands.Cog):
     async def reload_error(self, ctx, error):
         member = await ctx.author.create_dm()
         await member.send('I don\'t think you want to be trying that.')
+
+    @commands.command()
+    @commands.is_owner()
+    async def add_user(self, ctx, username, password, email, greeting='Hey ya\'ll'):
+        session = Session()
+        user = User()
+        user.username = username
+        user.password = password
+        user.email = email
+        user.greeting = greeting
+        user.created_at = datetime.now()
+
+        session.add(user)
+
+        try:
+            session.commit()
+            await ctx.send(f'Created user: {username}!')
+        except:
+            await ctx.send('That didn\'t work')
+
+        session.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def rm_user(self, ctx, username, email):
+        session = Session()
+        user = session.query(User).filter_by(username=username).filter_by(email=email).first()
+        await ctx.send(f"Removing {user.username} from the database")
+        session.delete(user)
+        session.commit()
+        session.close()
 
 def setup(bot):
     bot.add_cog(Admin(bot))
--- utils/db.py
+++ utils/db.py
@@ -1,31 +1,53 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer, Date, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy.orm import sessionmaker, relationship
 
-engine = create_engine('sqlite:///bot_db.db')
-session = sessionmaker(bind=engine)()
+engine = create_engine('sqlite:///bot_db.db', echo=True)
+Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
 class User(Base):
-
+    """User Model"""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
-    email = Column(String)
-    created_at = Column(Date)
+    id = Column('id', Integer, primary_key=True)
+    username = Column('username', String, unique=True)
+    password = Column('password', String)
+    email = Column('email', String)
+    greeting = Column('greeting', String)
+    created_at = Column('created_at', Date)
 
-    def __init__(self, user, email, password):
+    def __init__(self, user="", password="", email="", greeting="", date=""):
 
         self.user = user
         self.password = password
         self.email = email
-        self.created_at = 'date_placeholder'
+        self.greeting = greeting
+        self.created_at = date
 
     def __repr__(self):
-        return f'<User(name={self.user}, email={self.email}, password=*******, created_at={self.created_at})'
+        return f'<User(username="{self.user}", email="{self.email}", greeting="{self.greeting}" password="*******", created_at="{self.created_at}")'
+        
+class Promote(Base):
+    """Promote Model"""
+    __tablename__ = 'promotes'
+
+    id = Column('id', Integer, primary_key=True)
+    user_id = Column('user_id', Integer, ForeignKey('users.id'))
+    website = Column('website', String(120))
+    github = Column('github', String(50))
+    twitch = Column('twitch', String(50))
+    bio = Column('bio', Text())
+
+    def __init__(self, website, github, twitch, bio):
+        self.website = website
+        self.github = github
+        self.twitch = twitch
+        self.bio = bio
+
+    def __repr__(self):
+        return f'<Promote(user_id="{self.user_id}", website="{self.website}", twitch="{self.twitch}", bio="{self.bio}")'
+
 
 Base.metadata.create_all(engine)
