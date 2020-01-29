015214a1f831b9dd49cda593463ab79e02479947
Mike C <michael.e.case@gmail.com>
2020-01-28 22:30:26 -0600

    cleaned up admin code a bit

--- cogs/admin.py
+++ cogs/admin.py
@@ -14,14 +14,25 @@ class Admin(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def reload(self, ctx, *, module):
-        """ Reload module """
+        """ Reload module 
+        
+            Reloads the specified module or all modules if none defined. 
+            
+            [prefix]reload {module}
+            [prefix]reload 
+            
+        """
+        # make sure the response is always sent as a direct message to the user
         member = await ctx.author.create_dm()
+
         try:
-            self.bot.reload_extension(module)
+            print(f'Reloading {module} module...')
+            self.bot.reload_extension(f'cogs.{module}')
         except commands.ExtensionError as e:
             await member.send(f'{e.__class__.__name__}: {e}')
         else:
-            await member.send('\N{OK HAND SIGN}')
+            print(f'Module {module} reloaded successfully... ')
+            await member.send('\N{OK HAND SIGN}') # direct message 
 
     @reload.error
     async def reload_error(self, ctx, error):
@@ -30,7 +41,11 @@ class Admin(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def add_user(self, ctx, username, password, email, greeting='Hey ya\'ll'):
+    async def add_user(self, ctx, username, password, email, greeting=f'Hey!'):
+        """ Add a user to the database 
+
+            [prefix]add_user {username} {password} {email} [optional]{greeting}[/optional]
+        """
         session = Session()
         user = User()
         user.username = username
@@ -43,20 +58,34 @@ class Admin(commands.Cog):
 
         try:
             session.commit()
-            await ctx.send(f'Created user: {username}!')
         except:
             await ctx.send('That didn\'t work')
+        else:
+            await ctx.send(f'Created user: {username}!')
 
         session.close()
 
     @commands.command()
     @commands.is_owner()
     async def rm_user(self, ctx, username, email):
+        """ Remove user
+
+            Removes a user in the database
+            [prefix]rm_user {username} {user email}
+        """
+
         session = Session()
         user = session.query(User).filter_by(username=username).filter_by(email=email).first()
+        print(f"Removing {user.username} from the database")
         await ctx.send(f"Removing {user.username} from the database")
         session.delete(user)
-        session.commit()
+        try:
+            session.commit()
+        except:
+            await ctx.send("That didn't work.")
+        else:
+            print(f'{user.username} has been removed.')
+            await ctx.send(f'{user.username} has been removed.')
         session.close()
 
 def setup(bot):
--- cogs/shop.py
+++ cogs/shop.py
@@ -1,6 +1,8 @@
+import re
 import discord
 from discord.ext import commands
-from requests_html import HTMLSession
+import requests
+from bs4 import BeautifulSoup
 
 class Shopper(commands.Cog):
 
@@ -13,16 +15,23 @@ class Shopper(commands.Cog):
     @commands.command()
     async def amzn(self, ctx, *, search):
         """Search Amazon"""
-        
+        # Strip whitespace and insert + signs 
+        search_term = search.replace(' ', '+')
+
+        # set headers so amazon will crumble at our mighty...uhm... ok the thought ended there.. anyways.. 
+        headers = {
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+        }
         # @todo - fix this. Returns empty list
         # sure it has to do with the xpath 
-        r_session = HTMLSession()
-        
-        r = r_session.get(f'https://www.amazon.com/s?k={search}')
-        xpath = '//*[@id="search"]/div[1]/h2/a/@inner-html'
-        links = r.html.xpath(xpath)
-        print(links)
-        await ctx.send(links)
+        print(f'https://www.amazon.com/s?k={search_term}')
+        r = requests.get(f'https://www.amazon.com/s?k={search_term}', headers=headers)
+        # xpath = '//*[@id="search"]*/h2/a'
+        soup = BeautifulSoup(r.text, 'html.parser')
+        # links = r.status_code
+        print(soup)
+        # print(links.find('a'))
+        #await ctx.send(links)
 
     @commands.command()
     async def ebay(self, ctx, *, search):
